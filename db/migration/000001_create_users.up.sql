ffe8ac9d7356fa40f967a3cb30e625d793b88136
Vy Dao <vynguyendao@gmail.com>
2022-10-06 14:27:38 +0700

    Add feature create and get User info

--- db/migration/000001_create_users.up.sql
+++ db/migration/000001_create_users.up.sql
@@ -1,7 +1,8 @@
 CREATE TABLE "users" (
   "id" BIGSERIAL PRIMARY KEY,
   "username" varchar NOT NULL,
-  "password" char NOT NULL,
+  "password" varchar NOT NULL,
+  "score" float8 NOT NULL DEFAULT 0.0,
   "created_at" timestamp NOT NULL DEFAULT (now()),
   "updated_at" timestamp NOT NULL DEFAULT (now())
 );
